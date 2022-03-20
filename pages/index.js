d7bdc49c5c7158e9db29116ee59c1b117f387465
Ezzu99 <ezaan1999.ali@gmail.com>
2022-03-20 15:53:32 +0500

    added axios to login component

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import LoginForm from '../src/components/LoginForm';
 import { Button, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import Link from '@mui/material/Link';
+import Link from 'next/link';
 import { green } from '@mui/material/colors';
 
 const Index = () => {
@@ -14,8 +14,8 @@ const Index = () => {
           <Typography variant='h4' sx={{ color: 'gray' }}>At SafeHouse we not only improve society, but we help <Typography variant='body' color={green[800]} >create</Typography> one.</Typography>
           <Typography variant='h6' sx={{ marginTop: '8px', color: 'gray' }}>Interested in joining hands together?</Typography>
           <Box sx={{ display: 'flex', flexDirection: 'row', gap: '12px' }}>
-            <Link href='/AffiliationForm/NGO' underline='none'><Button size='large' variant="contained" sx={{ marginTop: '12px' }}>Affiliate with us</Button></Link>
-            <Link href='/' underline='none'><Button size='large' variant="outlined" sx={{ marginTop: '12px' }}>Discover More</Button></Link>
+            <Link href='/AffiliationForm/NGO'><Button size='large' variant="contained" sx={{ marginTop: '12px' }}>Affiliate with us</Button></Link>
+            <Link href='/'><Button size='large' variant="outlined" sx={{ marginTop: '12px' }}>Discover More</Button></Link>
           </Box>
         </Box>
       </Box>
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -7,6 +7,15 @@ import { Box } from "@mui/system";
 import { green } from '@mui/material/colors';
 import { styled } from '@mui/material/styles';
 
+let instance = axios.create({
+    baseURL: 'http://safehouse.herokuapp.com',
+    headers: {
+        post: {
+            'Content-Type': 'application/json'
+        }
+    }
+})
+
 const ColorButton = styled(Button)(({ theme }) => ({
     color: theme.palette.getContrastText(green[700]),
     backgroundColor: green[700],
@@ -23,8 +32,52 @@ const LoginForm = () => {
     const [username, setUsername] = React.useState('');
     const [password, setPassword] = React.useState('');
     const [selected, setSelected] = React.useState(false);
+    const [disableLogin, setDisableLogin] = React.useState(false);
     const [openErrorSnack, setOpenErrorSnack] = React.useState(false);
     const [openAlertSnack, setOpenAlertSnack] = React.useState(false);
+    const [userid, setUserid] = React.useState('');
+    const [loggedIn, setLoggedIn] = React.useState(false);
+    const [token, setToken] = React.useState('');
+    const [type, setType] = React.useState('');
+
+    const submitForm = async (e) => {
+        e.preventDefault();
+
+        setDisableLogin(true);
+
+        if (username == '' | password == '') {
+            setOpenAlertSnack(true);
+        }
+        else {
+            try {
+                let res = await instance.post('/v1/login/', {
+                    username,
+                    password
+                })
+                console.log(res);
+                setLoggedIn(true);
+                setToken(res.data.token);
+                setType(res.data.role);
+                setUsername(res.data.username);
+                setUserid(res.data.id);
+    
+                history.push('/dashboard');
+            }
+            catch (e) {
+                setOpenErrorSnack(true);
+            }
+        }
+
+        setDisableLogin(false);
+    }
+
+    React.useEffect(()  => {
+        localStorage.setItem('loggedIn', JSON.stringify(loggedIn));
+        localStorage.setItem('token', token);
+        localStorage.setItem('type', type);
+        localStorage.setItem('username', username);
+        localStorage.setItem('id', userid);
+    }, [loggedIn, token, type, username, userid])
 
     const handleClose = (event, reason) => {
         if (reason === 'clickaway') {
@@ -35,17 +88,6 @@ const LoginForm = () => {
         setOpenAlertSnack(false);
     };
 
-    const submitLogin = () => {
-        console.log(username, password);
-        
-        if (username == '' | password == '') {
-            setOpenAlertSnack(true);
-        }
-        else {
-            setOpenErrorSnack(true);
-        }
-    }
-
     return (
         <Box sx={{ height: '400px' }}>
             <Box sx={{ width: '400px', padding: '18px', bgcolor: 'white', borderRadius: '12px', boxShadow: '0 10px 28px #ddd', display: 'flex', flexDirection: 'column', gap: '12px' }}>
@@ -55,7 +97,7 @@ const LoginForm = () => {
                     <Checkbox checked={selected} onClick={() => { setSelected(!selected) }} />
                     <Typography sx={{ color: 'gray' }} onClick={() => { setSelected(!selected) }}>Remember me</Typography>
                 </Box>
-                <ColorButton size='large' variant='contained' onClick={submitLogin}>Login</ColorButton>
+                <ColorButton id='loginButton' size='large' variant='contained' onClick={submitForm} disabled={disableLogin}>Login</ColorButton>
                 <Typography color='primary' align='center'><Link href='/' underline='hover'>Forgotten Password?</Link></Typography>
             </Box>
             <Snackbar open={openErrorSnack} autoHideDuration={6000} onClose={handleClose}>
